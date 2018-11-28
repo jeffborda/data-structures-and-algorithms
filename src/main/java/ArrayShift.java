5caa50db7e610c486b06f656be324972619d1441
Jeff Borda <jeffborda@outlook.com>
2018-11-27 21:12:29 -0800

    commit for git issue

--- src/main/java/ArrayShift.java
+++ src/main/java/ArrayShift.java
@@ -4,7 +4,6 @@ public class ArrayShift {
     public static void main(String[] args) {
 
         //:::Tests:::
-
         // Odd number of elements
         int[] test1 = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(insertShiftArray(test1, 99)));
@@ -23,17 +22,15 @@ public class ArrayShift {
     }
 
     /**
-     * Takes in an array and the value to be added. Without utilizing any of the built-in methods available to your language, return an array with the new value added at the middle index.
-     * @param input
-     * @param insertionNumber
-     * @return
+     * Takes in an array and the value to be added. Without utilizing any of the built-in methods available to your
+     * language, return an array with the new value added at the middle index.
      */
     public static int[] insertShiftArray(int[] input, int insertionNumber) {
 
         int[] result = new int[input.length + 1];
         double middle = input.length / 2.0;
         middle += 0.5; //For rounding
-        middle = Math.floor(middle);
+        middle = Math.floor(middle); //Truncate the decimal
 
         for(int i = 0; i < input.length + 1; i++) {
 
@@ -50,6 +47,4 @@ public class ArrayShift {
 
         return result;
     }
-
-
 }
