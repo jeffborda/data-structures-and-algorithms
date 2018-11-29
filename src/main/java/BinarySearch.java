0e78ce36d91173645466863d11100e61f69245bb
Jeff Borda <jeffborda@outlook.com>
2018-11-29 09:23:36 -0800

    finished tests and added to readme file

--- src/main/java/BinarySearch.java
+++ src/main/java/BinarySearch.java
@@ -3,12 +3,12 @@ public class BinarySearch {
 
         int[] test = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 
-        System.out.println(binarySearch(test, 2));
+        System.out.println(binarySearch(test, 10));
     }
 
     public static int binarySearch(int[] sortedArray, int target) {
         int left = 0;
-        int right = sortedArray.length;
+        int right = sortedArray.length - 1;
         int middle = 0;
         while(left <= right) {
             middle = (int) Math.floor((left + right) / 2);
--- src/test/java/BinarySearchTest.java
+++ src/test/java/BinarySearchTest.java
@@ -5,7 +5,27 @@ import static org.junit.Assert.*;
 public class BinarySearchTest {
 
     @Test
-    public void binarySearch() {
-        int[] test = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+    public void testBinarySearch() {
+        int[] testArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+
+        assertEquals("binarySearch method should return 0, the index of target number", 0, BinarySearch.binarySearch(testArray, 1));
+        assertEquals("binarySearch method should return 12, the index of target number", 12, BinarySearch.binarySearch(testArray, 13));
+        assertEquals("binarySearch method should return 4, the index of target number", 4, BinarySearch.binarySearch(testArray, 5));
+        assertEquals("binarySearch method should return 9, the index of target number", 9, BinarySearch.binarySearch(testArray, 10));
+        assertEquals("binarySearch method should return -1, if target number not found", -1, BinarySearch.binarySearch(testArray, 99));
+
+        int[] smallArray = {1, 2};
+
+        assertEquals("binarySearch method should return 0, the index of target number", 0, BinarySearch.binarySearch(testArray, 1));
+        assertEquals("binarySearch method should return 1, the index of target number", 1, BinarySearch.binarySearch(testArray, 2));
+
+        int[] oneElementArray = {1};
+
+        assertEquals("binarySearch method should return 0, the index of target number", 0, BinarySearch.binarySearch(testArray, 1));
+
+
+        int[] emptyArray = new int[0];
+
+        assertEquals("binarySearch method should return 1, the index of target number", 1, BinarySearch.binarySearch(testArray, 2));
     }
 }
