9c6e4922d49b33024da72bcd746b90b0a4835356
Jeff Borda <jeffborda@outlook.com>
2018-12-10 09:08:11 -0800

    scaffolded new library and classes

---
+++ src/main/java/stacksandqueues/Queue.java
@@ -0,0 +1,12 @@
+package stacksandqueues;
+
+public class Queue {
+
+    Node head;
+    Node tail;
+
+    public Queue() {
+        this.head = null;
+        this.tail = null;
+    }
+}
---
+++ src/main/java/stacksandqueues/Stack.java
@@ -0,0 +1,12 @@
+package stacksandqueues;
+
+public class Stack {
+
+    Node top;
+
+    public Stack() {
+        this.top = null;
+    }
+
+
+}
