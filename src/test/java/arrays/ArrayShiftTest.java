8ff402a32c85f74e1f320e09bf9481ea5648908e
Jeff Borda <jeffborda@outlook.com>
2019-11-18 13:29:48 -0800

    refactored variable names in array shift tests

--- src/test/java/arrays/ArrayShiftTest.java
+++ src/test/java/arrays/ArrayShiftTest.java
@@ -11,37 +11,37 @@ public class ArrayShiftTest {
 
     @Test
     public void testInsertShiftArray_oddLengthInput() {
-        int[] test1 = {1, 2, 3, 4, 5};
-        int[] test1Expected = {1, 2, 3, 99, 4, 5};
-        assertArrayEquals("insertArrayShift should insert number 99 to the right of middle element.", test1Expected, ArrayShift.insertShiftArray(test1, 99));
+        int[] input = {1, 2, 3, 4, 5};
+        int[] expected = {1, 2, 3, 99, 4, 5};
+        assertArrayEquals("insertArrayShift should insert number 99 to the right of middle element.", expected, ArrayShift.insertShiftArray(input, 99));
     }
 
     @Test
     public void testInsertShiftArray_evenLengthInput() {
-        int[] test2 = {1, 2, 3, 4};
-        int[] test2Expected = {1, 2, 99, 3, 4};
-        assertArrayEquals("insertArrayShift should insert number 99 to the middle", test2Expected, ArrayShift.insertShiftArray(test2, 99));
+        int[] input = {1, 2, 3, 4};
+        int[] expected = {1, 2, 99, 3, 4};
+        assertArrayEquals("insertArrayShift should insert number 99 to the middle", expected, ArrayShift.insertShiftArray(input, 99));
     }
 
     @Test
     public void testInsertShiftArray_length1Input() {
-        int[] test3 = {1};
-        int[] test3Expected = {1, 99};
-        assertArrayEquals("insertArrayShift should insert number 99 to the middle", test3Expected, ArrayShift.insertShiftArray(test3, 99));
+        int[] input = {1};
+        int[] expected = {1, 99};
+        assertArrayEquals("insertArrayShift should insert number 99 to the middle", expected, ArrayShift.insertShiftArray(input, 99));
     }
 
     @Test
     public void testInsertShiftArray_length2Input() {
-        int[] test4 = {1, 2};
-        int[] test4Expected = {1, 99, 2};
-        assertArrayEquals("insertArrayShift should insert number 99 to the middle", test4Expected, ArrayShift.insertShiftArray(test4, 99));
+        int[] input = {1, 2};
+        int[] expected = {1, 99, 2};
+        assertArrayEquals("insertArrayShift should insert number 99 to the middle", expected, ArrayShift.insertShiftArray(input, 99));
     }
 
     @Test
     public void testInsertShiftArray_largerOddLengthInput() {
-        int[] test5 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
-        int[] test5Expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 99, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
-        assertArrayEquals("insertArrayShift should insert number 99 to the middle", test5Expected, ArrayShift.insertShiftArray(test5, 99));
+        int[] input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
+        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 99, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
+        assertArrayEquals("insertArrayShift should insert number 99 to the middle", expected, ArrayShift.insertShiftArray(input, 99));
     }
 
     @Test
