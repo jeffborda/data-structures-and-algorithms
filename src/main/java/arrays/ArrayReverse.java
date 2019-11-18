f1a72d2f98ca70632058664075e6ea24a6e56efe
Jeff Borda <jeffborda@outlook.com>
2019-11-18 13:21:39 -0800

    reverse array refactoring complete

--- src/main/java/arrays/ArrayReverse.java
+++ src/main/java/arrays/ArrayReverse.java
@@ -5,11 +5,8 @@ import java.util.Arrays;
 public class ArrayReverse {
 
     public static String[] reverseArray(String[] original) {
-
         String[] result = new String[original.length];
-
         for(int i = 0; i < original.length; i++) {
-
             result[i] = original[original.length - 1 - i];
         }
         return result;
