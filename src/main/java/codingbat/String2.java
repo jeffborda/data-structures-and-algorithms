0a8e41a1777642935fbe77002cdcd63789d04e65
Jeff Borda <jeffborda@outlook.com>
2019-11-22 10:22:26 -0800

    added package for codingbat, a class for string-2 questions, junit file for testing, starting method mixStrings

---
+++ src/main/java/codingbat/String2.java
@@ -0,0 +1,20 @@
+package codingbat;
+
+/**
+ * RE: https://codingbat.com/java/String-2
+ * This section contains medium level String coding challenges.
+ */
+public class String2 {
+
+    /**
+     * RE: https://codingbat.com/prob/p125185
+     * Given two strings, a and b, create a bigger string made of the first char of a, the first char of b, the second
+     * char of a, the second char of b, and so on. Any leftover chars go at the end of the result.
+     */
+    public static String mixString(String a, String b) {
+
+        return null;
+    }
+
+
+}
---
+++ src/test/java/codingbat/String2Test.java
@@ -0,0 +1,16 @@
+package codingbat;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class String2Test {
+
+
+    @Test
+    public void mixStringTest_1() {
+
+    }
+
+}
