9bd667684779ffde13fd49a7e62145bef296e44f
Jeff Borda <jeffborda@outlook.com>
2019-11-22 10:25:01 -0800

    mixStrings method written

--- src/main/java/codingbat/String2.java
+++ src/main/java/codingbat/String2.java
@@ -12,8 +12,35 @@ public class String2 {
      * char of a, the second char of b, and so on. Any leftover chars go at the end of the result.
      */
     public static String mixString(String a, String b) {
+        if(a.isEmpty() || b.isEmpty()) {
+            if(a.isEmpty()) {
+                return b;
+            }
+            return a;
+        }
 
-        return null;
+        StringBuilder answer = new StringBuilder();
+
+        if(a.length() <= b.length()) {
+            int i;
+            for(i = 0; i < a.length(); i++) {
+                answer.append(String.valueOf(a.charAt(i)));
+                answer.append(String.valueOf(b.charAt(i)));
+            }
+            if(i < b.length()) {
+                answer.append(b.substring(i, b.length()));
+            }
+            return answer.toString();
+        }
+        else {
+            int i;
+            for(i = 0; i < b.length(); i++) {
+                answer.append(String.valueOf(a.charAt(i)));
+                answer.append(String.valueOf(b.charAt(i)));
+            }
+            answer.append(a.substring(i, a.length()));
+            return answer.toString();
+        }
     }
 
 
