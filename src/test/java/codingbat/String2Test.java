2596f94a6abec0790abb6898fdc570a940c3cf12
Jeff Borda <jeffborda@outlook.com>
2019-11-22 10:31:44 -0800

    three tests written

--- src/test/java/codingbat/String2Test.java
+++ src/test/java/codingbat/String2Test.java
@@ -10,7 +10,17 @@ public class String2Test {
 
     @Test
     public void mixStringTest_1() {
+        assertEquals("axbycz", String2.mixString("abc", "xyz"));
+    }
 
+    @Test
+    public void mixStringTest_2() {
+        assertEquals("HTihere", String2.mixString("Hi", "There"));
+    }
+
+    @Test
+    public void mixStringTest_3() {
+        assertEquals("xTxhxexre", String2.mixString("xxxx", "There"));
     }
 
 }
