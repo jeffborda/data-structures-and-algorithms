e987f0badb3b3087dde487f2cb712775cee878fd
Jeff Borda <jeffborda@outlook.com>
2019-11-24 09:45:41 -0800

    added reference

--- src/main/java/codingbat/String2.java
+++ src/main/java/codingbat/String2.java
@@ -7,6 +7,7 @@ package codingbat;
 public class String2 {
 
     /**
+     * RE: https://codingbat.com/prob/p165312
      * Given a string, return a string where for every char in the original, there are two chars.
      */
     public static String doubleChar(String str) {
