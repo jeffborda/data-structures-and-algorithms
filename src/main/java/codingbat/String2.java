1d759bb38f36dcd0fa7ab754cd37b77920d23a8b
Jeff Borda <jeffborda@outlook.com>
2019-11-25 12:04:24 -0800

    code complete

--- src/main/java/codingbat/String2.java
+++ src/main/java/codingbat/String2.java
@@ -37,9 +37,17 @@ public class String2 {
      *  Return true if the string "cat" and "dog" appear the same number of times in the given string.
      */
     public static boolean catDog(String str) {
-
-        return false;
-
+        int catCount = 0;
+        int dogCount = 0;
+        for(int i = 0; i < str.length() - 2; i++) {
+            if(str.substring(i, i + 3).equals("dog") ) {
+                dogCount++;
+            }
+            else if(str.substring(i, i + 3).equals("cat")) {
+                catCount++;
+            }
+        }
+        return catCount == dogCount;
     }
 
     /**
