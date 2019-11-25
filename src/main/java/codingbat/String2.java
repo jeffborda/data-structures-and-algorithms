495456209312537bfe6b48a854afb13b4c6565ce
Jeff Borda <jeffborda@outlook.com>
2019-11-25 11:33:48 -0800

    code written, tests passing

--- src/main/java/codingbat/String2.java
+++ src/main/java/codingbat/String2.java
@@ -24,8 +24,13 @@ public class String2 {
      *  Return the number of times that the string "hi" appears anywhere in the given string.
      */
     public static int countHi(String str) {
-
-        return 0;
+        int answer = 0;
+        for(int i = 0; i < str.length() - 1; i++) {
+            if(str.substring(i, i + 2).equals("hi")) {
+                answer++;
+            }
+        }
+        return answer;
     }
 
     /**
