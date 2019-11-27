85496a62bb4aa10302ad5fb6bef19cc53add5bcb
Jeff Borda <jeffborda@outlook.com>
2019-11-26 16:11:50 -0800

    code challenge complete

--- src/main/java/codingbat/String2.java
+++ src/main/java/codingbat/String2.java
@@ -72,8 +72,8 @@ public class String2 {
      *    str.toLowerCase() returns the lowercase version of a string.
      */
     public static boolean endOther(String a, String b) {
-
-        return false;
+        // Ridiculous one liner :)
+        return a.length() >= b.length() ? a.substring(a.length() - b.length()).toLowerCase().equals(b.toLowerCase()) : b.substring(b.length() - a.length()).toLowerCase().equals(a.toLowerCase());
     }
 
 
