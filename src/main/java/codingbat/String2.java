8d63672cf1fb89d803b3aaaf7217d5deaf467a15
Jeff Borda <jeffborda@outlook.com>
2019-11-28 10:43:30 -0800

    reference, description, method signature

--- src/main/java/codingbat/String2.java
+++ src/main/java/codingbat/String2.java
@@ -161,4 +161,15 @@ public class String2 {
         return answer.toString();
     }
 
+    /**
+     *  RE: https://codingbat.com/prob/p128796
+     *  Given a string and an int n, return a string made of the first n characters of the string, followed by the first
+     *    n-1 characters of the string, and so on. You may assume that n is between 0 and the length of the string,
+     *    inclusive (i.e. n >= 0 and n <= str.length()).
+     */
+    public static String repeatFront(String str, int n) {
+
+        return null;
+    }
+
 }
