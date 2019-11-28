83d0700f5ee0eb05372ee19f0adad9e48590c267
Jeff Borda <jeffborda@outlook.com>
2019-11-27 20:19:49 -0800

    reference, description, and method signature

--- src/main/java/codingbat/String2.java
+++ src/main/java/codingbat/String2.java
@@ -103,6 +103,16 @@ public class String2 {
         return false;
     }
 
+    /**
+     *  RE: https://codingbat.com/prob/p134250
+     *  We'll say that a String is xy-balanced if for all the 'x' chars in the string, there exists a 'y' char
+     *    somewhere later in the string. So "xxy" is balanced, but "xyx" is not. One 'y' can balance multiple 'x's.
+     *    Return true if the given string is xy-balanced.
+     */
+    public static boolean xyBalance(String str) {
+
+        return false;
+    }
 
     /**
      * RE: https://codingbat.com/prob/p125185
