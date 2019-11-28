753310eb982d9597b335669aec547a805923c908
Jeff Borda <jeffborda@outlook.com>
2019-11-27 20:15:29 -0800

    code challenge complete

--- src/main/java/codingbat/String2.java
+++ src/main/java/codingbat/String2.java
@@ -96,7 +96,10 @@ public class String2 {
      *  Return true if the given string contains a "bob" string, but where the middle 'o' char can be any char.
      */
     public static boolean bobThere(String str) {
-
+        for(int i = 0; i < str.length() - 2; i++) {
+            if(str.charAt(i) == 'b' && str.charAt(i + 2) == 'b')
+                return true;
+        }
         return false;
     }
 
