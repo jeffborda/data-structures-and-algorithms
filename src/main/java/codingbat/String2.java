1121f2ec0df93c7c35604733988295ecb8dbfdef
Jeff Borda <jeffborda@outlook.com>
2019-12-04 12:40:43 -0800

    reference, description, method signature. also changed indents of java docs

--- src/main/java/codingbat/String2.java
+++ src/main/java/codingbat/String2.java
@@ -55,7 +55,7 @@ public class String2 {
     /**
      *  RE: https://codingbat.com/prob/p123614
      *  Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any
-     *    letter for the 'd', so "cope" and "cooe" count.
+     *   letter for the 'd', so "cope" and "cooe" count.
      */
     public static int countCode(String str) {
         int answer = 0;
@@ -70,8 +70,8 @@ public class String2 {
     /**
      *  RE: https://codingbat.com/prob/p126880
      *  Given two strings, return true if either of the strings appears at the very end of the other string, ignoring
-     *    upper/lower case differences (in other words, the computation should not be "case sensitive"). Note:
-     *    str.toLowerCase() returns the lowercase version of a string.
+     *   upper/lower case differences (in other words, the computation should not be "case sensitive"). Note:
+     *   str.toLowerCase() returns the lowercase version of a string.
      */
     public static boolean endOther(String a, String b) {
         // Ridiculous one liner :)
@@ -81,7 +81,7 @@ public class String2 {
     /**
      *  RE: https://codingbat.com/prob/p136594
      *  Return true if the given string contains an appearance of "xyz" where the xyz is not directly preceded by a
-     *    period (.). So "xxyz" counts but "x.xyz" does not.
+     *   period (.). So "xxyz" counts but "x.xyz" does not.
      */
     public static boolean xyzThere(String str) {
         for(int i = 0; i < str.length() - 2; i++) {
@@ -108,8 +108,8 @@ public class String2 {
     /**
      *  RE: https://codingbat.com/prob/p134250
      *  We'll say that a String is xy-balanced if for all the 'x' chars in the string, there exists a 'y' char
-     *    somewhere later in the string. So "xxy" is balanced, but "xyx" is not. One 'y' can balance multiple 'x's.
-     *    Return true if the given string is xy-balanced.
+     *   somewhere later in the string. So "xxy" is balanced, but "xyx" is not. One 'y' can balance multiple 'x's.
+     *   Return true if the given string is xy-balanced.
      */
     public static boolean xyBalance(String str) {
 
@@ -119,7 +119,7 @@ public class String2 {
     /**
      * RE: https://codingbat.com/prob/p125185
      * Given two strings, a and b, create a bigger string made of the first char of a, the first char of b, the second
-     *   char of a, the second char of b, and so on. Any leftover chars go at the end of the result.
+     *  char of a, the second char of b, and so on. Any leftover chars go at the end of the result.
      */
     public static String mixString(String a, String b) {
         if(a.isEmpty() || b.isEmpty()) {
@@ -153,7 +153,7 @@ public class String2 {
     /**
      *  RE: https://codingbat.com/prob/p152339
      *  Given a string and an int n, return a string made of n repetitions of the last n characters of the string. You
-     *    may assume that n is between 0 and the length of the string, inclusive.
+     *   may assume that n is between 0 and the length of the string, inclusive.
      */
     public static String repeatEnd(String str, int n) {
         StringBuilder answer = new StringBuilder();
@@ -166,8 +166,8 @@ public class String2 {
     /**
      *  RE: https://codingbat.com/prob/p128796
      *  Given a string and an int n, return a string made of the first n characters of the string, followed by the first
-     *    n-1 characters of the string, and so on. You may assume that n is between 0 and the length of the string,
-     *    inclusive (i.e. n >= 0 and n <= str.length()).
+     *   n-1 characters of the string, and so on. You may assume that n is between 0 and the length of the string,
+     *   inclusive (i.e. n >= 0 and n <= str.length()).
      */
     public static String repeatFront(String str, int n) {
         StringBuilder answer = new StringBuilder();
@@ -193,8 +193,8 @@ public class String2 {
     /**
      *  RE: https://codingbat.com/prob/p136417
      *  Given a string, consider the prefix string made of the first N chars of the string. Does that prefix string
-     *    appear somewhere else in the string? Assume that the string is not empty and that N is in the range
-     *    1..str.length().
+     *   appear somewhere else in the string? Assume that the string is not empty and that N is in the range
+     *   1..str.length().
      */
     public static boolean prefixAgain(String str, int n) {
         // first call to substring() returns the substring from n to the end of string
@@ -204,7 +204,7 @@ public class String2 {
     /**
      *  RE: https://codingbat.com/prob/p159772
      *  Given a string, does "xyz" appear in the middle of the string? To define middle, we'll say that the number of
-     *    chars to the left and right of the "xyz" must differ by at most one. This problem is harder than it looks.
+     *   chars to the left and right of the "xyz" must differ by at most one. This problem is harder than it looks.
      */
     public static boolean xyzMiddle(String str) {
         // Eliminate short strings and strings not containing 'xyz'
@@ -227,8 +227,8 @@ public class String2 {
     /**
      *  RE: https://codingbat.com/prob/p129952
      *  A sandwich is two pieces of bread with something in between. Return the string that is between the first and
-     *    last appearance of "bread" in the given string, or return the empty string "" if there are not two pieces of
-     *    bread.
+     *   last appearance of "bread" in the given string, or return the empty string "" if there are not two pieces of
+     *   bread.
      */
     public static String getSandwich(String str) {
         String targetWord = "bread";
@@ -252,7 +252,7 @@ public class String2 {
     /**
      *  RE: https://codingbat.com/prob/p194491
      *  Returns true if for every '*' (star) in the string, if there are chars both immediately before and after the
-     *    star, they are the same.
+     *   star, they are the same.
      */
     public static boolean sameStarChar(String str) {
         for(int i = 1; i < str.length() - 1; i++) {
@@ -261,6 +261,17 @@ public class String2 {
             }
         }
         return true;
+    }
+
+    /**
+     *  RE: https://codingbat.com/prob/p122943
+     *  Given a string, compute a new string by moving the first char to come after the next two chars, so "abc" yields
+     *   "bca". Repeat this process for each subsequent group of 3 chars, so "abcdef" yields "bcaefd". Ignore any group
+     *   of fewer than 3 chars at the end.
+     */
+    public static String oneTwo(String str) {
+
+        return null;
     }
 
 }
