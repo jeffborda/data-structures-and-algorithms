fb46527894aa440bee81bd2e524a2474e1ebf76f
Jeff Borda <jeffborda@outlook.com>
2019-12-04 14:37:59 -0800

    reference, description, method signature

--- src/main/java/codingbat/String2.java
+++ src/main/java/codingbat/String2.java
@@ -293,4 +293,14 @@ public class String2 {
         return answer.toString();
     }
 
+    /**
+     *  RE: https://codingbat.com/prob/p180759
+     *  Look for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'. Return
+     *   a string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".
+     */
+    public static String zipZap(String str) {
+
+        return null;
+    }
+
 }
