06ba216d99793caef7be4e1343defb67467e61f5
Jeff Borda <jeffborda@outlook.com>
2019-12-07 23:24:56 -0800

    reference, description, method signature

--- src/main/java/codingbat/String2.java
+++ src/main/java/codingbat/String2.java
@@ -357,4 +357,15 @@ public class String2 {
         return answer.toString();
     }
 
+    /**
+     *  RE: https://codingbat.com/prob/p147538
+     *  Given a string and a non-empty word string, return a string made of each char just before and just after every
+     *   appearance of the word in the string. Ignore cases where there is no char before or after the word, and a char
+     *   may be included twice if it is between two words.
+     */
+    public static String wordEnds(String str, String word) {
+
+        return null;
+    }
+
 }
