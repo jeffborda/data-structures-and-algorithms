8965789b0db9b01b36f1f85033facb984560108d
Jeff Borda <jeffborda@outlook.com>
2019-12-08 23:08:16 -0800

    empty tests:

--- src/test/java/codingbat/String3Test.java
+++ src/test/java/codingbat/String3Test.java
@@ -1,5 +1,7 @@
 package codingbat;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
@@ -8,6 +10,60 @@ import static org.junit.Assert.*;
  */
 public class String3Test {
 
+    // RE: https://codingbat.com/prob/p199171
+    @Test
+    public void countYZTest_01() {
 
+    }
 
+    @Test
+    public void countYZTest_02() {
+
+    }
+
+    @Test
+    public void countYZTest_03() {
+
+    }
+
+    @Test
+    public void countYZTest_04() {
+
+    }
+
+    @Test
+    public void countYZTest_05() {
+
+    }
+
+    @Test
+    public void countYZTest_06() {
+
+    }
+
+    @Test
+    public void countYZTest_07() {
+
+    }
+
+    @Test
+    public void countYZTest_08() {
+
+    }
+
+    @Test
+    public void countYZTest_09() {
+
+    }
+
+    @Test
+    public void countYZTest_10() {
+
+    }
+
+    @Test
+    public void countYZTest_11() {
+
+    }
+    
 }
