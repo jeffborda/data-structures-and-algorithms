24b22f4672f6a3fda775f17e88af98af2e55a303
Jeff Borda <jeffborda@outlook.com>
2019-12-09 21:28:26 -0800

    reference, description, method signature

--- src/main/java/codingbat/String3.java
+++ src/main/java/codingbat/String3.java
@@ -23,4 +23,15 @@ public class String3 {
         }
         return count;
     }
+
+    /**
+     *  RE: https://codingbat.com/prob/p192570
+     *  Given two strings, 'base' and 'remove', return a version of the base string where all instances of the remove
+     *   string have been removed (not case sensitive). You may assume that the remove string is length 1 or more.
+     *   Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
+     */
+    public static String withoutString(String base, String remove) {
+
+        return null;
+    }
 }
