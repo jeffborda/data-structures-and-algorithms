123be3d01b31e20d4fa47d6ca9c060bb436311cb
Jeff Borda <jeffborda@outlook.com>
2020-01-16 20:06:02 -0800

    wrote corrected algorithm: isConcatentation

--- src/main/java/codingbat/OtherString.java
+++ src/main/java/codingbat/OtherString.java
@@ -13,7 +13,6 @@ public class OtherString {
      *   Note: This code will fail under certain circumstances, ex: "aab", "aaab"  (see test #6). Will require double
      *    for loop. Leaving this as-is for learning purposes.
      */
-    @SuppressWarnings("SpellCheckingInspection")
     public static boolean isConcat(String str1, String str2) {
 
         int j = 0;
@@ -24,6 +23,27 @@ public class OtherString {
                 j = 0;
             }
             if(j == str1.length()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     *  Corrected version of the above, using double for loop to prevent the error which occurs with repeated char's.
+     */
+    public static boolean isConcatenation(String str1, String str2) {
+
+        int correctChars = 0;
+        for(int i = 0; i < str2.length() - str1.length() + 1; i++) {
+            for(int j = 0; j < str1.length(); j++) {
+                if(str2.charAt(i + j) == str1.charAt(j)) {
+                    correctChars++;
+                } else {
+                    correctChars = 0;
+                }
+            }
+            if(correctChars == str1.length()) {
                 return true;
             }
         }
--- src/test/java/codingbat/OtherStringTest.java
+++ src/test/java/codingbat/OtherStringTest.java
@@ -37,4 +37,34 @@ public class OtherStringTest {
     public void isConcatTest_06() {
         assertTrue(OtherString.isConcat("aab", "aaab"));
     }
+
+    @Test
+    public void isConcatenationTest_01() {
+        assertTrue(OtherString.isConcatenation("cat", "concatenation"));
+    }
+
+    @Test
+    public void isConcatenationTest_02() {
+        assertTrue(OtherString.isConcatenation("age", "pilgrimage"));
+    }
+
+    @Test
+    public void isConcatenationTest_03() {
+        assertTrue(OtherString.isConcatenation("dog", "dogma"));
+    }
+
+    @Test
+    public void isConcatenationTest_04() {
+        assertFalse(OtherString.isConcatenation("hello", "world"));
+    }
+
+    @Test
+    public void isConcatenationTest_05() {
+        assertTrue(OtherString.isConcatenation("aba", "alabama"));
+    }
+
+    @Test
+    public void isConcatenationTest_06() {
+        assertTrue(OtherString.isConcatenation("aab", "aaab"));
+    }
 }
